07129f184b40c2de1a3d9168d9cbb2ef6e7af362
ashwin37 <ashwint379@gmail.com>
2017-01-25 12:20:46 -0500

    Initial Commit

---
+++ 3DTouch_Sample/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  3DTouch_Sample
+//
+//  Created by Ashwin Tallapaka on 1/25/17.
+//  Copyright © 2017 Ashwin Tallapaka. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
